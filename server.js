54a93ceeb55babc7aea2085919ebece1c95f4f03
udhayasankar07 <udhaaya007@gmail.com>
2016-10-10 22:56:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,59 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var mySite={
+    title:'gadgets',
+    heading:'Galaxy J7',
+    date:'oct 10 2016',
+    content: `
+               <p>
+                The Samsung Galaxy J7 Prime has been launched in India at a price of Rs. 18,790 and is now available at various retail stores. It has a lot of features such as a metal body, large 3300mAh battery, S Power Planning, and superb screen. S Power Planning and Secure Folder are both neat value additions in software. Despite featuring a large 5.5-inch screen, the J7 Prime is easy to manage. At this price point, the phone is pleasure to use.
+
+                   The new Samsung J-series handset will easily compete with current favourites such as the Motorola Moto X Play and Lenovo Vibe X3. The Galaxy J7 Prime however faces its biggest challenge with the launch of the Lenovo Z2 Plus which packs a flagship-grade Snapdragon 820 processor and starts at a price of Rs. 17,999.
+               </p>`
+}
+function createTemplate(data)
+{
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+var htmlTemplate=`
+<html>
+    <head>
+        ${title}
+        <meta name="viewport" content="width=device-width, initial -scale=1" />
+        <link href="ui/style.css" rel="stylesheet"/>
+    </head>
+    
+    <body>
+        <div class="container">
+             <div>
+            <a href="/">Home</a>
+        </div>
+            
+        <h1> 
+       ${heading}
+        </h1>
+        
+        <div >
+            <img src="/ui/madi.png" class="img-medium"/>
+        </div>
+        
+       
+        <hr>
+        <div>
+            ${date}
+        </div>
+        
+        <div>
+            
+        
+        
+            ${content}
+         </div>`;
+         return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -14,7 +67,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/udhaya', function (req, res) {
-  res.sendfile(path.join(__dirname,'ui','udhaya.html'));
+  res.send(createTemplate(mySite));
 });
 
 app.get('/udhaya1', function (req, res) {
