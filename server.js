6ee66f0c1a0d72fdfd1da6f61ed7d2452d1c7ad8
udhayasankar07 <udhaaya007@gmail.com>
2016-10-10 23:33:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,9 +82,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/:newudhaya', function (req, res) {
-    var newudhaya=req.params.newudhaya;
-  res.send(createTemplate(myFile[newudhaya]));
+app.get('/:newUdhaya', function (req, res) {
+    var newUdhaya=req.params.newUdhaya;
+  res.send(createTemplate(myFile[newUdhaya]));
 });
 
 
