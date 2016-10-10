ac83025961affc79f4333355de8c1423dffa0750
udhayasankar07 <udhaaya007@gmail.com>
2016-10-10 19:27:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/walak', function (req, res) {
+  res.send(path.join('udhaya is active'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
