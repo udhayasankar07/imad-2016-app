d0c56b990353898add1d845df6d846ddad413ae4
udhayasankar07 <udhaaya007@gmail.com>
2016-10-11 20:25:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ alert("Are you interested in mobiles");
             image.onclick=function()
             {
                 
-                var interval=setInterval(moveRight,500);
+                var interval=setInterval(moveRight,100);
                 
             }
             function moveRight()
