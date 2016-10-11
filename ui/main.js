4dea3e169571ee15561c59318b3d3f16fcb2e6c3
udhayasankar07 <udhaaya007@gmail.com>
2016-10-11 20:15:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,16 @@ console.log('Loaded!');
 alert("Are you interested in mobiles");
             var element=document.getElementById('main')
             element.innerHTML='Gadgets Wala'
+            
+            var marginleft=0;
+            image.onclick=function()
+            {
+                var interval=setInterval(moveRight,500);
+                
+            }
+            function moveRight()
+            {
+                marginleft=marginleft+1;
+            image.style.marginLeft=marginleft+'px';
+                
+            }
