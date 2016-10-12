91269a6f11796906b9ec9a4306e18ec5bbf533c2
udhayasankar07 <udhaaya007@gmail.com>
2016-10-12 13:44:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -55,8 +55,7 @@ console.log('Loaded!');
               
             };
             
-            var nameInput=document.getElementById('name');
-            var name=nameInput.value;
+            
             var submit=document.getElementById('submit_bth');
             submit.onclick=function()
             {
