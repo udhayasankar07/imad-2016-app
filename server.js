7fe55d6980bb55b010f8a77df056894c13193bec
udhayasankar07 <udhaaya007@gmail.com>
2016-10-12 10:47:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,6 +91,14 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
+var names=[];
+app.get('/submit-name', function (req, res) {
+  var name=req.query.name;
+  names.push(name);
+  res.send(JSON.stringify(names));
+});
+
+
 app.get('/:newUdhaya', function (req, res) {
     var newUdhaya=req.params.newUdhaya;
   res.send(createTemplate(myFile[newUdhaya]));
@@ -101,12 +109,7 @@ app.get('/:newUdhaya', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-var names=[];
-app.get('/submit-name', function (req, res) {
-  var name=req.query.name;
-  names.push(name);
-  res.send(JSON.stringify(names));
-});
+
 
 
 
