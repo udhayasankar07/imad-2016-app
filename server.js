a89546a494b78750f4dc083eb5d66956c41cdbab
udhayasankar07 <udhaaya007@gmail.com>
2016-11-03 23:01:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,9 +126,27 @@ app.get('/submit-name', function (req, res) {
 });
 
 
-app.get('/:newUdhaya', function (req, res) {
-    var newUdhaya=req.params.newUdhaya;
-  res.send(createTemplate(myFile[newUdhaya]));
+app.get('/articles/:articleName', function (req, res) {
+    
+   pool.query("SELECT * FROM article WHERE title='" +req.params.articleName +"'",function(err,result){
+       if(err)
+    {
+        res.status(500).send(err.toString());
+    }
+    else
+    {
+     if(result.rows.length===0)   
+     {
+          res.status(404).send('article not found');
+     }
+    else
+    {
+        var articleData=result.rows[0];
+        res.send(createTemplate(articleData));
+    }
+    }   
+   });
+  
 });
 
 
