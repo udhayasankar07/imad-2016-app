3bb6aa5528345f7fa0e532ba8e9fe13da019980c
udhayasankar07 <udhaaya007@gmail.com>
2016-11-03 21:07:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+
+var config = {
+  host: 'db.imad.hasura-app.io',
+  user: 'udhayasankar07',
+  password: process.env.DB_PASSWORD,
+  database: 'udhayasankar07',
+  port: '5432'
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -85,6 +94,24 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+
+var pool = new Pool(config);
+
+app.get('/test-db', function (req, res) {
+  pool.query('SELECT * FROM test',function(err,result)
+  {
+    if(err)
+    {
+        res.status(500).send(err.toString());
+    }
+    else
+    {
+        res.send(JSON.stringify(result));
+    }
+      
+  });
+});
+
 var counter=0;
 app.get('/counter', function (req, res) {
     counter=counter+1;
