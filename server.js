e85d094c27526556f532579c2a336b521f439d8f
udhayasankar07 <udhaaya007@gmail.com>
2016-11-04 21:52:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
   host: 'db.imad.hasura-app.io',
@@ -69,6 +70,20 @@ var htmlTemplate=`
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+
+});
+
+
+function hash (input, salt) {
+    // How do we create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+}
+
+
+app.get('/hash/:input', function(req, res) {
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
 });
 
 
