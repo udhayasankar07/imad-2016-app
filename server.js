e8c23ff7bb6e2ff76ba3c8d180c3939fca2ee3c0
udhayasankar07 <udhaaya007@gmail.com>
2016-11-04 21:16:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
   host: 'db.imad.hasura-app.io',
@@ -14,6 +16,11 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
+}));
 
 function createTemplate(data)
 {
