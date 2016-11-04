82e89d4d2253321e26fc46bebbdef461d31e3ba2
udhayasankar07 <udhaaya007@gmail.com>
2016-11-04 23:04:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -150,6 +150,23 @@ app.post('/login', function (req, res) {
 
 
 
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
+});
+
+
+
 
 
 app.get('/index', function (req, res) {
