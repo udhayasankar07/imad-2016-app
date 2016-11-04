3d33b52ea2526ecf33c0123f63b52723cd489f52
udhayasankar07 <udhaaya007@gmail.com>
2016-11-04 23:18:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -140,7 +140,7 @@ var submit = document.getElementById('login_btn');
         var password = document.getElementById('password').value;
         console.log(username);
         console.log(password);
-        request.open('POST', '/login', true);
+        request.open('POST', 'http://udhayasankar07.imad.hasura-app.io/login', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
         
