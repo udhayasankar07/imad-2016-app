7aa5f232994417fedaef94af8a6b4e1996b4bfa1
udhayasankar07 <udhaaya007@gmail.com>
2016-11-05 10:34:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,11 +115,10 @@ app.post('/create-user', function (req, res) {
 });
 
 
-app.post('/login/:input', function (req, res) {
+app.get('/login/:input', function (req, res) {
    var input=req.params.input.split('$');
    var username = input[0];
    var password = input[1];
-   res.send(''+input);
    pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
