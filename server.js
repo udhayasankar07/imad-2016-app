cb04bd5903261322e400a8be8552ec652cac7050
udhayasankar07 <udhaaya007@gmail.com>
2016-11-11 22:26:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -175,7 +175,7 @@ app.get('/check-login', function (req, res) {
 
 app.get('/logout', function (req, res) {
    delete req.session.auth;
-   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
+   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a> <body background="http://blog.hostbaby.com/wp-content/uploads/2014/03/PaintSquares_1920x1234.jpg"></body></html>');
 });
 
 
