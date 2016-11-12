61fadafc61f59dde2c78f9e64897650f3cca07a5
udhayasankar07 <udhaaya007@gmail.com>
2016-11-12 11:12:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,32 +1,4 @@
 console.log('Loaded!');
-
-          
-            
-            var marginleft=0;
-          
-           var image=document.getElementById('image');
-            
-            image.onclick=function()
-            {
-              
-               
-                var interval=setInterval(moveRight,1000);
-                
-                
-                
-            };
-            function moveRight()
-            {
-               
-               
-                marginleft=marginleft+20;
-            image.style.marginLeft=marginleft+'px';
-                
-               
-                
-                
-            }
-            
             var button=document.getElementById('counter');
             
             button.onclick=function()
