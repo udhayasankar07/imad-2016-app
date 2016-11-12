56b5d745ef1d1be3ca40618bf7c02782c500c8d7
udhayasankar07 <udhaaya007@gmail.com>
2016-11-12 07:53:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -152,16 +152,19 @@ function loginFunction () {
 function registerFunction () {
         // Create a request object
         var request = new XMLHttpRequest();
-        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        var input=username+'$'+password;
         // Capture the response and store it in a variable
         request.onreadystatechange = function () {
           if (request.readyState === XMLHttpRequest.DONE) {
               // Take some action
               if (request.status === 200) {
-                 alert('logged in Successfully');
+                 alert('User '+ username+' created successfully');
                   window.location.href='http://udhayasankar07.imad.hasura-app.io/home';
-              } else if (request.status === 403) {
-                  alert('Username/password incorrect');
               } else if (request.status === 500) {
                   alert('Something went wrong on the server');
                   
@@ -169,13 +172,6 @@ function registerFunction () {
           }  
          
         };
-        
-        // Make the request
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
-        var input=username+'$'+password;
         request.open('GET', 'http://udhayasankar07.imad.hasura-app.io/register/'+input, true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(null);  
