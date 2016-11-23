00082dd70712eddfdd9480b5407527f0478b712f
udhayasankar07 <udhaaya007@gmail.com>
2016-11-23 17:08:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ console.log('Loaded!');
                         {
                             var counter =request.responseText;
                             var span=document.getElementById('count');
-                            span.innerHTML=counter.toString();
+                            span.innerHTML=counter;
                         }
                     }
                 };
