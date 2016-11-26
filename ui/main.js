c0579fbe4f472fbcbc5bf4c5aef6e1b53a6dbd26
udhayasankar07 <udhaaya007@gmail.com>
2016-11-26 18:25:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,31 +31,7 @@ request.send(null);
 
 
 
-            var button=document.getElementById('counter');
-            
-            button.onclick=function()
-            {
-                var request=new XMLHttpRequest();
-                request.onreadystatechange=function()
-                {
-                    if(request.readyState===XMLHttpRequest.DONE)
-                    {
-                        if(request.status===200)
-                        {
-                            var counter =request.responseText;
-                            var span=document.getElementById('count');
-                            span.innerHTML=counter.toString();
-                        }
-                    }
-                };
-                
-                request.open('GET','http://udhayasankar07.imad.hasura-app.io/counter',true);
-                request.send(null);
-             
-              
-              
-            };
-            
+           
             
             
             
