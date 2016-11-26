605fa4eb38ca5a0c33c834736b2077aaad79fd9b
udhayasankar07 <udhaaya007@gmail.com>
2016-11-26 17:59:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -57,38 +57,7 @@ request.send(null);
             };
             
             
-            var submit=document.getElementById('submit_bth');
-            submit.onclick=function()
-            {
-                 var nameInput=document.getElementById('name');
-        var name=nameInput.value;
-                
-                  var request=new XMLHttpRequest();
-                request.onreadystatechange=function()
-                {
-                    if(request.readyState===XMLHttpRequest.DONE)
-                    {
-                        if(request.status===200)
-                        {
-                            var names=request.responseText;
-                            names=JSON.parse(names);
-                                var list='';
-                     for(var i=0;i<names.length;i++)
-                    {
-                    list+='<li>' + names[i] +'</li>';
-                    }
-                    var ul=document.getElementById('namelist');
-                     ul.innerHTML=list;
-                        }
-                    }
-                };
-                
-                request.open('GET','http://udhayasankar07.imad.hasura-app.io/submit-name?name='+name,true);
-                request.send(null);
-                
-                
-               
-            };
+            
             
             
             
